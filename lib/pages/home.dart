5962a9f4bc95530f76744d9bcd1db06feb2c22d2
rume edgar <rumeedgar@icloud.com>
2023-04-02 11:22:03 +0000

    app update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,10 +15,51 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool _loading = true;
+  File _image;
+  List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState(){
+    super.initState();
+    loadModel().theen((value){
+      setState(() {
+        
+      });
+    });
+  }
+
+  detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 3,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async{
+    await Tflite.loadModel(model: 'assets/model')
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  pickImage() async{
+    var image = await picker.getImage(source: source)
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.blueAccent,
         body: Container(
             padding: EdgeInsets.symmetric(horizontal: 24),
             child: Column(
@@ -25,18 +68,72 @@ class _HomePageState extends State<HomePage> {
                 SizedBox(height: 50),
                 Text(
                   'edgar studios',
-                  style: TextStyle(color: Color(0x7D9E9E), fontSize: 20),
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
                 SizedBox(height: 5),
                 Text(
                   'cocoa disease Detector',
                   style: TextStyle(
-                      color: Color(0x7D9E9E),
+                      color: Colors.white,
                       fontWeight: FontWeight.w500,
                       fontSize: 30),
                 ),
                 SizedBox(height: 50),
-                Center(child: _loading)
+                Center(
+                  child: _loading
+                      ? Container(
+                          width: 400,
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/cocoa.png'),
+                              SizedBox(height: 50),
+                            ],
+                          ),
+                        )
+                      : Container(),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 250,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                          decoration: BoxDecoration(
+                            color: Colors.brown,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text(
+                            'Capture a Photo',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 250,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                          decoration: BoxDecoration(
+                            color: Colors.brown,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text(
+                            'Upload a Photo',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ))
         // body: SafeArea(
